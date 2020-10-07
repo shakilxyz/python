fd5f3fc0912b582c1e90c3f7419161281d0f02ce
shakilxyz <49758596+shakilxyz@users.noreply.github.com>
2020-10-07 21:30:42 +0600

    Add files via upload

---
+++ projects/beginner/rock-paper-scessior.py
@@ -0,0 +1,28 @@
+import random
+
+lst = ['rock', 'paper', 'scissors']
+
+while True:
+    computer = lst[random.randint(0, 2)]
+    player = input('Rock / Paper / Scissors ? : ').lower()
+
+    if player == computer:
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "scissors":
+        if computer == "rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+    print()
